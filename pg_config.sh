5b5a7e7d2426f1b4807c831e149c30554dd8b99a
Abdul-Karim <mmkarim52@gmail.com>
2017-06-10 10:33:21 -0400

    Added vagrant setup, all the frontend pages and python files

---
+++ pg_config.sh
@@ -0,0 +1,19 @@
+apt-get -qqy update
+apt-get -qqy upgrade
+apt-get -qqy install postgresql python-psycopg2
+apt-get -qqy install python-sqlalchemy
+apt-get -qqy install python-pip
+pip install --upgrade pip
+pip install werkzeug==0.8.3
+pip install flask==0.9
+pip install Flask-Login==0.1.3
+pip install oauth2client
+pip install requests
+pip install httplib2
+apt-get -qqy install python3 python3-pip
+apt-get -qqy install postgresql python3-psycopg2
+apt-get -qqy install python3-sqlalchemy
+apt-get -qqy install python3-pip
+python -m pip install tornado
+
+
